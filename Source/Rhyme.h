42153cca2e16d2bacf3d5d7d65f9dda0b1f0ff4a
Artem <artem@a627.ru>
2023-10-16 23:21:17 +0300

    Delete build/

--- Source/Rhyme.h
+++ Source/Rhyme.h
@@ -5,5 +5,10 @@
 inline bool IsRhyme(char *line1, char *line2) {
   // std::cout << line1 << '\n' << line2 << '\n';
 
+  char *line1_end = line1;
+  while (*line1_end != '\0') {
+    ++line1_end;
+  }
+
   return false;
 }
